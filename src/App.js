b6067a8829083934172044f6dc6cbad9cfa5ef2b
Paweł Choniawko <pawel.choniawko@gmail.com>
2023-01-09 23:30:08 +0100

    r3f shader

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useMemo } from "react";
+
+import * as THREE from "three";
+
+const fragmentShader = `
+uniform vec2 u_resolution;
+        uniform float u_time;
+
+        void main() {
+            vec2 st = gl_FragCoord.xy/u_resolution;
+            gl_FragColor=vec4(st.x,st.y,0.0,1.0);
+        }
+`;
 
 function App() {
+  const ref = useRef();
+
+  const data = useMemo(
+    () => ({
+      uniforms: {
+        u_time: { type: "f", value: 1.0 },
+        u_resolution: {
+          type: "v2",
+          value: new THREE.Vector2(window.innerWidth + 600, window.innerHeight + 500),
+        },
+        u_mouse: { type: "v2", value: new THREE.Vector2() },
+      },
+      fragmentShader,
+    }),
+    [],
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <mesh ref={ref}>
+      <planeGeometry args={[1, 1, 32, 32]} />
+      <shaderMaterial attach='material' {...data} />
+    </mesh>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Canvas } from "@react-three/fiber";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
 root.render(
-  <React.StrictMode>
+  <Canvas
+    shadows
+    camera={{
+      fov: 45,
+      near: 0.1,
+      far: 200,
+      position: [0.0, 0.0, 1.5],
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Canvas>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
